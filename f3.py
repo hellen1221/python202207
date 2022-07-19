d38663fea2f8f89fe3e238b1ea26e27c1a21afef
hellen1221 <hsrhee00@gmail.com>
2022-07-19 09:46:18 +0900

    f4

---
+++ f3.py
@@ -0,0 +1,12 @@
+#function3.py
+
+a=1.2
+print(id(a))
+a=2.3
+print(id(a))
+
+
+lst = [1,2,3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
---
+++ f4.py
@@ -0,0 +1,8 @@
+# function4.py
+
+def times(a=10, b=20):
+    return a*b
+
+print(times())
+print(times(5))
+print(times(5,6))
