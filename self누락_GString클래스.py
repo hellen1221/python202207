f676235dd1216e9473d7c0937a81a4f1e8ab7030
hellen1221 <hsrhee00@gmail.com>
2022-07-19 13:51:15 +0900

    버그수정

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,14 @@
+#전역변수 
 str = "Not Class Member"
+
+#클래스의정의 : 파이썬에서는 항상 명시적으로 지정하자 self.str
 class GString:
     def __init__(self):
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        print(self.str)
 
 g = GString()
 g.set("First Message")
