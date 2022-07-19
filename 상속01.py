f9be8bfa4a449d3ee2956d2ec2ada7d657911917
hellen1221 <hsrhee00@gmail.com>
2022-07-19 15:36:54 +0900

    상속내것_강사것확인

--- 상속01.py
+++ 상속01.py
@@ -6,13 +6,22 @@ class Person:
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+# 자식클래스
+# class Student(Person):
+#     def __init__(self, name, phoneNumber, subject, studentID):
+#         self.name = name
+#         self.phoneNumber = phoneNumber
+#         self.subject = subject
+#         self.studentID = studentID
+
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
