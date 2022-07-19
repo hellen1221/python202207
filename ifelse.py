6926ed995eb7fbd6b373b0fef6052fc57c10c1d5
hellen1221 <hsrhee00@gmail.com>
2022-07-19 11:25:19 +0900

    조건연습

--- ifelse.py
+++ ifelse.py
@@ -13,13 +13,35 @@
 
 # print("등급은 ", grade)
 
-value =5
-while value > 0:
-    print(value)
-    value -= 1
+# value =5
+# while value > 0:
+#     print(value)
+#     value -= 1
 
-lst = [1,2,3]
-for item in lst:
+# lst = [1,2,3]
+# for item in lst:
+#     print(item)
+
+
+
+lst = list(range(1,11))
+print([i**2 for i in lst if i > 5]) 
+
+lst = [10, 30, 500]
+iterL = filter(None, lst)
+for item in iterL:
     print(item)
- 
-    
+
+def getB20(i):
+    return i > 20
+
+print("필터링")
+iterL = filter(getB20, lst)
+for item in iterL:
+    print(item)
+
+print("람다함수")
+iterL = filter(lambda x:x>20, lst)
+for item in iterL:
+    print(item)
+
