e68ee7c55082d0ae1705cdf6e123554937184d48
hellen1221 <hsrhee00@gmail.com>
2022-07-19 11:13:38 +0900

    조건연습

---
+++ ifelse.py
@@ -0,0 +1,25 @@
+# ifelse.py
+# 다중라인 ^+/
+# score = int(input("점수입력"))
+
+# if 90 <= score <= 100:
+#     grade = 'A'
+# elif 80 <= score < 90:
+#     grade = 'B'
+# elif 70 <= score < 80:
+#     grade = 'C'
+# else:
+#     grade = 'D'
+
+# print("등급은 ", grade)
+
+value =5
+while value > 0:
+    print(value)
+    value -= 1
+
+lst = [1,2,3]
+for item in lst:
+    print(item)
+ 
+    
